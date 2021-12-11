bf7c4a01fad6b93051193c7bf7492e83ff16f897
juwon <juwon8893@naver.com>
2021-12-12 00:59:00 +0900

    프론트단 작업

--- app/app.js
+++ app/app.js
@@ -9,5 +9,5 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/",home); // 미들웨어 등록해주는 메소드.
-
+app.use(express.static(`${__dirname}/src/public`));
 module.exports = app;
